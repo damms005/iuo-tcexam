ef5bc46f040d4ce10fa8e5066264871ace179efa
Damilola Olowookere <damms005@gmail.com>
2019-07-01 16:18:03 +0100

    added quick download via html table download

--- admin/code/tce_show_result_allusers.php
+++ admin/code/tce_show_result_allusers.php
@@ -418,10 +418,37 @@ if (isset($_REQUEST['sel'])) {
 
     if ($itemcount > 0) {
         echo '<div class="row">' . K_NEWLINE;
+
         // show buttons by case
-        echo '<a href="tce_xml_results.php?menu_mode=startlongprocess' . $filter . '" class="xmlbutton" title="' . $l['h_xml_export'] . '">XML</a> ';
-        echo '<a href="tce_xml_results.php?format=JSON&amp;menu_mode=startlongprocess' . $filter . '" class="xmlbutton" title="JSON">JSON</a> ';
-        echo '<a href="tce_tsv_result_allusers.php?' . $filter . '&amp;order_field=' . urlencode($order_field) . '&amp;orderdir=' . $orderdir . '" class="xmlbutton" title="' . $l['h_tsv_export'] . '">TSV</a> ';
+
+        ?>
+
+        <script src="../../shared/jscripts/xlsx.core.min.js"></script>
+        <script src="../../shared/jscripts/FileSaver.js"></script>
+        <script src="../../shared/jscripts/tableexport.js"></script>
+
+        <script>
+        TableExport(document.getElementsByClassName("userselect")[0] , {
+        headers: true,                      // (Boolean), display table headers (th or td elements) in the <thead>, (default: true)
+        footers: true,                      // (Boolean), display table footers (th or td elements) in the <tfoot>, (default: false)
+        formats: ["xlsx", "csv", "txt"],    // (String[]), filetype(s) for the export, (default: ['xlsx', 'csv', 'txt'])
+        filename: "cbt - " + new Date().toISOString(),                     // (id, String), filename for the downloaded file, (default: 'id')
+        bootstrap: false,                   // (Boolean), style buttons using bootstrap, (default: true)
+        exportButtons: true,                // (Boolean), automatically generate the built-in export buttons for each of the specified formats (default: true)
+        position: "bottom",                 // (top, bottom), position of the caption element relative to table, (default: 'bottom')
+        ignoreRows: null,                   // (Number, Number[]), row indices to exclude from the exported file(s) (default: null)
+        ignoreCols: null,                   // (Number, Number[]), column indices to exclude from the exported file(s) (default: null)
+        trimWhitespace: true,               // (Boolean), remove all leading/trailing newlines, spaces, and tabs from cell text in the exported file(s) (default: false)
+        RTL: false,                         // (Boolean), set direction of the worksheet to right-to-left (default: false)
+        sheetname: "cbt"                     // (id, String), sheet name for the exported spreadsheet, (default: 'id')
+        });
+        </script>
+
+        <?php
+
+        // echo '<a href="tce_xml_results.php?menu_mode=startlongprocess' . $filter . '" class="xmlbutton" title="' . $l['h_xml_export'] . '">XML</a> ';
+        // echo '<a href="tce_xml_results.php?format=JSON&amp;menu_mode=startlongprocess' . $filter . '" class="xmlbutton" title="JSON">JSON</a> ';
+        // echo '<a href="tce_tsv_result_allusers.php?' . $filter . '&amp;order_field=' . urlencode($order_field) . '&amp;orderdir=' . $orderdir . '" class="xmlbutton" title="' . $l['h_tsv_export'] . '">TSV</a> ';
         echo '<a href="tce_pdf_results.php?mode=1' . $filter . '" class="xmlbutton" title="' . $l['h_pdf'] . '">' . $l['w_pdf'] . '</a> ';
         echo '<a href="tce_pdf_results.php?mode=4' . $filter . '" class="xmlbutton" title="' . $l['h_pdf_all'] . '">' . $l['w_pdf_all'] . '</a> ';
         if (K_DISPLAY_PDFTEXT_BUTTON) {
@@ -448,6 +475,7 @@ echo '</div>' . K_NEWLINE;
 
 echo '<div class="pagehelp">' . $l['hp_result_alluser'] . '</div>' . K_NEWLINE;
 echo '</div>';
+
 
 require_once '../code/tce_page_footer.php';
 
