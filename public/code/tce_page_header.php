f98f07d590ac3a2dbc3a031a51d06e58532a0a38
Damilola Olowookere <damms005@gmail.com>
2019-07-10 20:39:26 +0100

    re-styled the user info card

--- public/code/tce_page_header.php
+++ public/code/tce_page_header.php
@@ -55,46 +55,39 @@ require_once dirname(__FILE__) . '/tce_page_menu.php';
 
 echo '<div class="body">' . K_NEWLINE;
 
-echo "<div id='userLcd' class='z-10 fixed mr-2 p-2 bg-white' >";
-?>
-            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+if (isset($_SESSION['session_user_firstname']) && !empty($_SESSION['session_user_firstname'] ) ) {
+    echo "<div id='userLcd' class='z-10 fixed p-4 float-right cursor-pointer' >";
+    ?>
+            <div class="flex flex-column inner rounded overflow-hidden shadow-lg m-auto bg-white">
                 <?php
-if (is_file("../../shared/config/passports/" . @$_SESSION['session_user_passport'])) {
-    echo "<img class='w-full' src='../../shared/config/passports/{$_SESSION['session_user_passport']}' />";
-}
-?>
+    if (is_file("../../shared/config/passports/" . @$_SESSION['session_user_passport'])) {
+        echo "<img class='mt-2' src='../../shared/config/passports/{$_SESSION['session_user_passport']}' />";
+    }
+    ?>
                 <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2"><?php echo @$_SESSION['session_user_firstname'] . @$_SESSION['session_user_lastname']; ?></div>
+                    <div class="font-bold text-lg mb-2"><?php echo @$_SESSION['session_user_firstname'] . ' ' . @$_SESSION['session_user_lastname']; ?></div>
                     <p class="text-grey-darker text-base">
-                        <?php
-echo "<span>
-                                                        (" . @$_SESSION['session_user_name'] . ")
-                                                    </span>
-                                                    <br>
-                                                    <span>
-                                                    </span>
-                                                    <br>
-                                                    <span>
-                                                        " . @$_SESSION['session_user_department'] . "
-                                                    </span>
-                                                    <br>
-                                                    <span>
-                                                        " . @$_SESSION['session_user_year_level'] . " LEVEL
-                                                    </span>";
-?>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                     </p>
                 </div>
                 <div class="px-6 py-4">
-                    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#photography</span>
-                    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#travel</span>
-                    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#winter</span>
+                    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">
+                    <?php echo @$_SESSION['session_user_name']; ?>
+                    </span>
+                    <br>
+                    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">
+                    <?php echo @$_SESSION['session_user_department']; ?>
+                    </span>
+                    <br>
+                    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">
+                    <?php echo (!isset( $_SESSION['session_user_year_level'] ) ? "" : $_SESSION['session_user_year_level'] . ' Level'); ?>
+                    </span>
                 </div>
             </div>
 
         <?php
 echo "
 </div>";
+}
 
 echo '<a name="topofdoc" id="topofdoc"></a>' . K_NEWLINE;
 echo '<h1>' . htmlspecialchars($thispage_title, ENT_NOQUOTES, $l['a_meta_charset']) . '</h1>' . K_NEWLINE;
