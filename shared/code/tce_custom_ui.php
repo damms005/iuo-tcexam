fe5d554411c679cbece2cd68e679c1e9371ca064
Damilola Olowookere <damms005@gmail.com>
2021-03-10 10:52:17 +0100

    Fix custom UI

---
+++ shared/code/tce_custom_ui.php
@@ -0,0 +1,9 @@
+<?php
+
+echo '<link rel="stylesheet" href="' . K_PATH_STYLE_SHEETS . '../../shared/css/custom.css" type="text/css" />' . K_NEWLINE;
+echo '<script src="' . K_PATH_URL . 'shared/jscripts/jquery.js"></script>';
+echo '<script src="' . K_PATH_URL . 'shared/jscripts/popper.js"></script>';
+echo '<script src="' . K_PATH_URL . 'shared/jscripts/bootstrap.min.js"></script>';
+echo '<script type="text/javascript" src="' . K_PATH_SHARED_JSCRIPTS . '../../shared/jscripts/custom.js"></script>' . K_NEWLINE;
+
+?>
